714f8c601b7b0d2ce792718325bd9135b7d9c7e7
akmin1226 <i007min92@gmail.com>
2026-05-22 19:11:13 +0900

    auto deploy

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Docker Hub + Azure ACR CI/CD"
+    return "Version 2 Auto Deploy Success!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
